432c11751d1aa3c9370ce18217b0fc06027d3e8e
Pritam Kumar Behera <67405430+pritam377@users.noreply.github.com>
2025-03-05 11:30:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 provider "aws" {
 region = "us-east-1"
 }
+resource "aws_instance" "server" {
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hcp-server"
+  }
+}
 
