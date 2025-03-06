46db593fbd24e22995590ce83d44d9965b531caf
Pritam Kumar Behera <67405430+pritam377@users.noreply.github.com>
2025-03-06 15:52:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "server" {
-  count         = 5
+  count         = 1
   ami           = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   tags = {
